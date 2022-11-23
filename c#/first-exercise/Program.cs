33fb8bafb5693421765c7ab50e218b23c2061ed1
Carol Vilar <bo.varig@gmail.com>
2022-11-23 20:36:35 -0300

    array exercise

---
+++ c#/first-exercise/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] nome = new string[3];
+      
+      for (int i = 0; i < 3; i++)
+      {
+        Console.WriteLine("Digite um nome: ");
+        nome[i] = Console.ReadLine();
+      }
+    }
+  }
+}
