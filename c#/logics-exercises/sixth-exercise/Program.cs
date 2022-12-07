1cb65c548122074e9d5c0ea2e7262011495bc7e6
Carol Vilar <bo.varig@gmail.com>
2022-12-07 20:01:04 -0300

    practicing arrays

---
+++ c#/logics-exercises/sixth-exercise/Program.cs
@@ -0,0 +1,11 @@
+﻿// using System.Threading.Tasks.Dataflow;
+namespace c_ {
+  class Program {
+    static void Main () {
+      string Word = Console.ReadLine();
+      for(int i = 0; i < Word.Length; i++) {
+        Console.Write(Word.ToUpper()[i] + "-");
+      }
+    }
+  }
+}
